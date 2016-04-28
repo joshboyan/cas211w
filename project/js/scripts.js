382bc44f7e5f8dd5fbb648395542607d4b2dc90c
Josh Boyan <joshboyan@yahoo.com>
2016-04-28 08:57:48 -0700

    Add JQuery .hover effects to feature and news area

---
+++ project/js/scripts.js
@@ -0,0 +1,48 @@
+$('#feature1').hover(
+    function(){ $('#feature1 .img-circle').addClass('hover') },
+    function(){ $('#feature1 .img-circle').removeClass('hover')}
+);
+$('#feature1').hover(
+    function(){ $('#feature1 .feature-heading').addClass('slide-up') },
+    function(){ $('#feature1 .feature-heading').removeClass('slide-up')}
+);
+$('#feature2').hover(
+    function(){ $('#feature2 .img-circle').addClass('hover') },
+    function(){ $('#feature2 .img-circle').removeClass('hover')}
+);
+$('#feature2').hover(
+    function(){ $('#feature2 .feature-heading').addClass('slide-up') },
+    function(){ $('#feature2 .feature-heading').removeClass('slide-up')}
+);
+$('#feature3').hover(
+    function(){ $('#feature3 .img-circle').addClass('hover') },
+    function(){ $('#feature3 .img-circle').removeClass('hover')}
+);
+$('#feature3').hover(
+    function(){ $('#feature3 .feature-heading').addClass('slide-up') },
+    function(){ $('#feature3 .feature-heading').removeClass('slide-up')}
+);
+$('#feature4').hover(
+    function(){ $('#feature4 .img-circle').addClass('hover') },
+    function(){ $('#feature4 .img-circle').removeClass('hover')}
+);
+$('#feature4').hover(
+    function(){ $('#feature4 .feature-heading').addClass('slide-up') },
+    function(){ $('#feature4 .feature-heading').removeClass('slide-up')}
+);
+$('#feature5').hover(
+    function(){ $('#feature5 .img-circle').addClass('hover') },
+    function(){ $('#feature5 .img-circle').removeClass('hover')}
+);
+$('#feature5').hover(
+    function(){ $('#feature5 .feature-heading').addClass('slide-up') },
+    function(){ $('#feature5 .feature-heading').removeClass('slide-up')}
+);
+$('#feature6').hover(
+    function(){ $('#feature6 .img-circle').addClass('hover') },
+    function(){ $('#feature6 .img-circle').removeClass('hover')}
+);
+$('#feature6').hover(
+    function(){ $('#feature6 .feature-heading').addClass('slide-up') },
+    function(){ $('#feature6 .feature-heading').removeClass('slide-up')}
+);
