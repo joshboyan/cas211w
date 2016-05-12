93192d1937aa1e9f99b0aec383065fb76fc5be6e
Josh Boyan <joshboyan@yahoo.com>
2016-05-12 12:14:17 -0700

    Add archive.php

---
+++ project/wp-content/themes/unveil/archive.php
@@ -0,0 +1,108 @@
+
+<?php get_header(); ?>
+
+<!-- Wrap the rest of the page in another container to center all the content. -->
+
+<div class="container marketing">
+<div class="row">
+    <!-- Content Area
+    ================================================== -->
+    <section id="content_area">
+        <div class="col-sm-8">
+            <h1>Content/Sidebar Page Layout</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam commodi consectetur culpa cupiditate debitis, dolore dolorum esse eum fugit id mollitia non placeat possimus provident quasi quibusdam rem tempora voluptas.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab dolorem ducimus id ipsam natus voluptatum. Commodi cumque minus odio placeat quidem. Architecto, expedita ipsam modi optio provident rem repellat voluptas! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquam dolore ducimus excepturi facilis iusto, maiores minima modi porro quae reiciendis repellat voluptate? Amet aspernatur, dolorem esse necessitatibus nostrum quia.</p>
+            <h2>A Secondary Header</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias deserunt dolorem esse, expedita fuga incidunt labore non placeat porro ratione reprehenderit sint temporibus. Aspernatur harum neque officia perferendis saepe temporibus.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dolores expedita nam nostrum odit quo repudiandae sunt temporibus. Architecto, consequuntur culpa delectus doloribus dolorum eaque inventore ipsa nostrum quisquam tempora.</p>
+
+            <div class="row">
+                <a href="#">
+                    <div class="col-md-6">
+                        <div class="row post_nav">
+                            <div class="col-xs-2">
+                                <i class="fa fa-chevron-left"></i>
+                            </div><!-- /.col-xs-2-->
+                            <div class="col-xs-6">
+                                <h4>Previous Post</h4>
+                                <h4>Latest Work</h4>
+                            </div><!-- /.col-xs-6-->
+                            <div class="col-xs-4">
+                                <img src="img/bento-thumb.jpg" alt="previous post featured image">
+                            </div><!-- /.col-xs-4-->
+                        </div><!-- /.row-->
+                    </div><!-- /.col-md-6 -->
+                </a>
+
+                <a href="#">
+
+                    <div class="col-md-6">
+                        <div class="row post_nav">
+                            <div class="col-xs-4">
+                                <img src="img/appetizers-thumb.jpg" alt="previous post featured image">
+                            </div><!-- /.col-xs-4-->
+                            <div class="col-xs-6">
+                                <h4>Next Post</h4>
+                                <h4>Show What You Want</h4>
+                            </div><!-- /.col-xs-6-->
+                            <div class="col-xs-2">
+                                <i class="fa fa-chevron-right"></i>
+                            </div><!-- /.col-xs-2-->
+
+                        </div><!-- /.row-->
+                    </div><!-- /.col-md-6 -->
+                </a>
+            </div> <!-- /.row-->
+        </div> <!-- /.col-sm-8 -->
+    </section><!-- /#content_area -->
+    <!-- Sidebar Area
+    ================================================== -->
+    <section id="sidebar">
+        <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
+            <div class="sidebar-module sidebar-module-inset">
+                <h4>About</h4>
+                <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+            </div><!-- /.sidebar-module -->
+            <div class="sidebar-module">
+                <h4>Archives</h4>
+                <ol class="list-unstyled">
+                    <li><a href="#">March 2014</a></li>
+                    <li><a href="#">February 2014</a></li>
+                    <li><a href="#">January 2014</a></li>
+                    <li><a href="#">December 2013</a></li>
+                    <li><a href="#">November 2013</a></li>
+                    <li><a href="#">October 2013</a></li>
+                    <li><a href="#">September 2013</a></li>
+                    <li><a href="#">August 2013</a></li>
+                    <li><a href="#">July 2013</a></li>
+                    <li><a href="#">June 2013</a></li>
+                    <li><a href="#">May 2013</a></li>
+                    <li><a href="#">April 2013</a></li>
+                </ol>
+            </div><!-- /.sidebar-module -->
+            <div class="sidebar-module">
+                <h4>Elsewhere</h4>
+                <ol class="list-unstyled">
+                    <li><a href="#">GitHub</a></li>
+                    <li><a href="#">Twitter</a></li>
+                    <li><a href="#">Facebook</a></li>
+                </ol>
+            </div><!-- /.sidebar-module -->
+        </div><!-- /.blog-sidebar -->
+        </sectio><!-- /#sidebar -->
+</div><!-- /.row -->
+
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+    <!-- POSTS SECTION -->
+    <article>
+        <h2><?php the_title(); ?></h2>
+        <p><?php the_excerpt(); ?></p>
+    </article>
+
+<?php endwhile; else : ?>
+    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
+
+<?php get_footer(); ?>
--- project/wp-content/themes/unveil/index.php
+++ project/wp-content/themes/unveil/index.php
@@ -2,73 +2,18 @@
 <?php get_header(); ?>
 
 <!-- Wrap the rest of the page in another container to center all the content. -->
-
 <div class="container marketing">
 
-<!-- Marketing messaging and featured work
-================================================== -->
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-    <section id="featured-work">
-    <h2>Featured Work</h2>
-    <!-- Three columns of text below the carousel -->
-    <div class="row">
-        <div class="col-sm-4" id="feature1">
-            <a href="#"><img class="img-circle" src="<?php echo esc_url(get_template_directory_uri() ); ?>/img/baguette.jpg" alt="Generic placeholder image"></a>
-            <h3 class="feature-heading">Most Important</h3>
-            <p>On mobile devices this will be the first thing users see after the carousel. This is where your most impressive offering should go. Gaze pattern mapping suggests this will generally be the first place desktop users look as well...</p>
-            <p><a class="btn" href="#" role="button">View details &raquo;</a></p>
-        </div><!-- /.col-sm-4 -->
-        <div class="col-sm-4" id="feature2">
-            <a href="#"><img class="img-circle" src="<?php echo esc_url(get_template_directory_uri() ); ?>/img/pierogis-small.jpg" alt="Generic placeholder image"></a>
-            <h3 class="feature-heading">What Are These?</h3>
-            <p>These 3 section link to the other main pages of your site. The top navigation is a last resort for users. You are guiding their journey with well curated materials and beautiful images. Did you see that hover effect creating great user experiences? Yeah you did...</p>
-            <p><a class="btn" href="#" role="button">View details &raquo;</a></p>
-        </div><!-- /.col-sm-4 -->
-        <div class="col-sm-4" id="feature3">
-            <a href="#"><img class="img-circle" src="<?php echo esc_url(get_template_directory_uri() ); ?>/img/grilled.jpg" alt="Generic placeholder image"></a>
-            <h3 class="feature-heading">Setting These Images</h3>
-            <p>Each of these sections links to a static page on your site. The picture is the featured image (and also the hero banner image) you set for that page. The heading that floats up over the picture is the title of that page. This text is in-fact the first text that appears on the page. All this lead to smooth page transitions and good UI...</p>
-            <p><a class="btn" href="#" role="button">View details &raquo;</a></p>
-        </div><!-- /.col-sm-4 -->
-    </div><!-- /.row -->
-    </section><!-- /featured work -->
+        <!-- POSTS SECTION -->
+        <article>
+            <h2><?php the_title(); ?></h2>
+            <p><?php the_excerpt(); ?></p>
+        </article>
 
-<!-- Shopping Banner
-================================================== -->
-    <section id="shopping">
-    <div class="container-fluid shopping-banner">
-        <div class="shopping-btn-container"><a role="button" class="btn btn-primary shopping-btn" href="#" role="button">Shop!</a></div>
-    </div>
-    </section><!-- /shopping-->
-
-<!-- Latest work and news
-================================================== -->
-    <section id="latest work and news">
-    <h2>Latest Work and News</h2>
-    <!-- Three columns of text below the carousel -->
-    <div class="row">
-        <div class="col-sm-4" id="feature4">
-            <a href="#"><img class="img-circle" src="<?php echo esc_url(get_template_directory_uri() ); ?>/img/bento.jpg" alt="Generic placeholder image"></a>
-            <h3 class="feature-heading">Latest Work</h3>
-            <p>Did you see that shop button gracefully appear? You can bet your users will. They probably won't make it past that but if they do they are going to get blown away by your latest poems about your vegan bicycle or whatever it is you do. This will be the first item in this section users see...</p>
-            <p><a class="btn" href="#" role="button">View details &raquo;</a></p>
-        </div><!-- /.col-sm-4 -->
-        <div class="col-sm-4" id="feature5">
-            <a href="#"><img class="img-circle" src="<?php echo esc_url(get_template_directory_uri() ); ?>/img/pinwheel-small.jpg" alt="Generic placeholder image"></a>
-            <h3 class="feature-heading">Curating This Section</h3>
-            <p>By default this section will show your last three posts. This section gets all of its images and copy from your latest post. Pretty easy huh? People are gonna think you are working so hard to maintain this masterpiece...</p>
-            <p><a class="btn" href="#" role="button">View details &raquo;</a></p>
-        </div><!-- /.col-sm-4 -->
-        <div class="col-sm-4"  id="feature6">
-            <a href="#"><img class="img-circle" src="<?php echo esc_url(get_template_directory_uri() ); ?>/img/appetizers.jpg" alt="Generic placeholder image"></a>
-            <h3 class="feature-heading">Show What You Want</h3>
-            <p>If you have a seasonal promotion or enter into your blue phase this section is easy to curate using the category tags you are diligently assigning to all your post. Please tell me your assigning categories! We are all busy and don't have the time for my long winded speech about SEO... </p>
-            <p><a class="btn" href="#" role="button">View details &raquo;</a></p>
-        </div><!-- /.col-sm-4 -->
-    </div><!-- /.row -->
-    </section><!-- /latest work and news -->
-
-<!-- Footer information
-================================================== -->
+    <?php endwhile; else : ?>
+        <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+    <?php endif; ?>
 
 <?php get_footer(); ?>
