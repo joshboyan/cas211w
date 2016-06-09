23e4c79f95ce3174d63c016b0d4a5b619e803b68
joshboyan <joshboyan@yahoo.com>
2016-06-08 23:48:19 -0700

    Create contact form with contact form7

--- project/wp-content/themes/unveil/page-tempates/two_half_page_sidebars.php
+++ project/wp-content/themes/unveil/page-tempates/two_half_page_sidebars.php
@@ -47,39 +47,10 @@ get_header(); ?>
                 <?php endif; ?>
                 </article>
 
-                <div role="form" class="wpcf7" id="wpcf7-f156-p77-o1" lang="en-US" dir="ltr">
+                <div class="well">
                     <div class="screen-reader-response"></div>
-                    <form action="/project/page-two-sidebars/#wpcf7-f156-p77-o1" method="post" class="wpcf7-form" novalidate="novalidate">
-                        <div style="display: none;">
-                            <input type="hidden" name="_wpcf7" value="156" />
-                            <input type="hidden" name="_wpcf7_version" value="4.4" />
-                            <input type="hidden" name="_wpcf7_locale" value="en_US" />
-                            <input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f156-p77-o1" />
-                            <input type="hidden" name="_wpnonce" value="426a916099" />
-                        </div>
-                        <div class="well">
-                            <p>Send us a message</p>
-                            <div class="form-group"><label class="sr-only" for="input-name">Name</label><br />
-                                <span class="wpcf7-form-control-wrap your-name"><input type="text" name="your-name" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required form-control" id="input-name" aria-required="true" aria-invalid="false" placeholder="Name" /></span>
-                            </div>
-                            <p> <!-- /form-group --></p>
-                            <div class="form-group">
-                                <label class="sr-only" for="input-subject">Name</label><br />
-                                <span class="wpcf7-form-control-wrap your-subject"><input type="text" name="your-subject" value="" size="40" class="wpcf7-form-control wpcf7-text form-control" id="input-subject" aria-invalid="false" placeholder="Subject" /></span>
-                            </div>
-                            <p> <!-- /form-group --></p>
-                            <div class="form-group"><label class="sr-only" for="input-email">Email</label><br />
-                                <span class="wpcf7-form-control-wrap your-email"><input type="email" name="your-email" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email form-control" id="input-email" aria-required="true" aria-invalid="false" placeholder="Email" /></span>
-                            </div>
-                            <p> <!-- /form-group --></p>
-                            <div class="form-group"><label class="sr-only" for="input-message">Message</label><br />
-                                <span class="wpcf7-form-control-wrap your-message"><textarea name="your-message" cols="40" rows="3" class="wpcf7-form-control wpcf7-textarea wpcf7-validates-as-required form-control" id="input-message" aria-required="true" aria-invalid="false" placeholder="Message"></textarea></span>
-                            </div>
-                            <p> <!-- /form-group --><br />
-                                <input type="submit" value="Click to Send" class="wpcf7-form-control wpcf7-submit btn btn-default" />
-                        </div>
-                        <p>  <!-- /well --></p>
-                        <div class="wpcf7-response-output wpcf7-display-none"></div></form></div>
+
+                    <?php echo do_shortcode( '[contact-form-7 id="115" title="Contact form 1"]' ); ?>
 
             </div><!-- /.row -->
 
